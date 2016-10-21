99bfba5b0fba104d056eab8524f390cde47fb16f
qk <qk@detu.com>
2016-10-21 22:36:35 +0800

    web phalcon init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # game
 棋牌游戏项目
+
+## 目录结构
+
+websocket
+
+web
+
+## websocket系统
+
+采用swoole框架搭建，运行系统得安装swoole扩展库。
+
+## web系统
+
+采用phalcon框架搭建，运行系统得安装phalcon扩展库。
---
+++ web/public/index.php
@@ -0,0 +1,36 @@
+<?php
+
+try {
+
+    //Register an autoloader
+    $loader = new \Phalcon\Loader();
+    $loader->registerDirs(array(
+        '../app/controllers/',
+        '../app/models/'
+    ))->register();
+
+    //Create a DI
+    $di = new Phalcon\DI\FactoryDefault();
+
+    //Setup the view component
+    $di->set('view', function(){
+        $view = new \Phalcon\Mvc\View();
+        $view->setViewsDir('../app/views/');
+        return $view;
+    });
+
+    //Setup a base URI so that all generated URIs include the "tutorial" folder
+    $di->set('url', function(){
+        $url = new \Phalcon\Mvc\Url();
+        $url->setBaseUri('/tutorial/');
+        return $url;
+    });
+
+    //Handle the request
+    $application = new \Phalcon\Mvc\Application($di);
+
+    echo $application->handle()->getContent();
+
+} catch(\Phalcon\Exception $e) {
+     echo "PhalconException: ", $e->getMessage();
+}
